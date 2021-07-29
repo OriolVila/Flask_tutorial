80dad83c83f3ffc1bd261b055902aca0abb4d498
OriolVila <oriolvm@gmail.com>
2021-07-29 19:20:39 +0200

    Adding SQLite and templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -10,6 +10,13 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
